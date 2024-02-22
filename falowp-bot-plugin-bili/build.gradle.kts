9ba9e4730c3d83585888ffc242c28c350dd628c1
冰璃然 <bingliran@126.com>
2024-02-22 11:24:21 +0800

    feat(plugins): 升级一些依赖版本

--- falowp-bot-plugin-bili/build.gradle.kts
+++ falowp-bot-plugin-bili/build.gradle.kts
@@ -2,5 +2,5 @@ dependencies {
     implementation("com.blr19c.falowp:falowp-bot-system:${project.version}")
     implementation("com.blr19c.falowp:falowp-bot-plugin-auth:${project.version}")
     //二维码
-    implementation("com.google.zxing:javase:3.5.2")
+    implementation("com.google.zxing:javase:3.5.3")
 }
--- falowp-bot-plugin-idle/build.gradle.kts
+++ falowp-bot-plugin-idle/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     implementation("com.blr19c.falowp:falowp-bot-system:${project.version}")
     //日期处理
-    implementation("org.mnode.ical4j:ical4j:4.0.0-rc1")
+    implementation("org.mnode.ical4j:ical4j:4.0.0-rc3")
 }
