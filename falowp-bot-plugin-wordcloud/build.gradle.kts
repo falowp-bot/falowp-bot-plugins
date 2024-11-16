13f3344fb9d59db61b3ed643161a260713ccbf3d
冰璃然 <bingliran@126.com>
2024-11-16 23:13:01 +0800

    feat(plugins): 更新依赖版本

--- falowp-bot-plugin-wordcloud/build.gradle.kts
+++ falowp-bot-plugin-wordcloud/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("com.blr19c.falowp:falowp-bot-system:${project.version}")
     implementation(project(":falowp-bot-utils-db"))
     //分词器
-    implementation("com.hankcs:hanlp:portable-1.8.4") {
+    implementation("com.hankcs:hanlp:portable-1.8.5") {
         exclude("junit", "junit")
     }
 }
