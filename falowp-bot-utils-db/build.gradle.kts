9010b371a853991364b3486d63d501e71a9072a7
冰璃然 <bingliran@126.com>
2024-12-14 17:10:39 +0800

    feat(plugins): 不再默认提供数据库driver

--- falowp-bot-utils-db/build.gradle.kts
+++ falowp-bot-utils-db/build.gradle.kts
@@ -1,4 +1,4 @@
-val exposedVersion = "0.56.0"
+val exposedVersion = "0.57.0"
 
 dependencies {
     implementation("com.blr19c.falowp:falowp-bot-system:${project.version}")
@@ -7,7 +7,4 @@ dependencies {
     api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     api("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
     runtimeOnly("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    api("org.mariadb.jdbc:mariadb-java-client:3.5.1")
-    api("com.mysql:mysql-connector-j:9.1.0")
-    api("org.postgresql:postgresql:42.7.4")
 }
