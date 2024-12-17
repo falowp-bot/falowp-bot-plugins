327a652c1c717f51578a9da98ea9710a45b25748
冰璃然 <bingliran@126.com>
2024-12-17 21:04:05 +0800

    feat(plugins): 更新版本

--- falowp-bot-plugin-idle/build.gradle.kts
+++ falowp-bot-plugin-idle/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     implementation("com.blr19c.falowp:falowp-bot-system:${project.version}")
     //日期处理
-    implementation("org.mnode.ical4j:ical4j:4.0.6")
+    implementation("org.mnode.ical4j:ical4j:4.0.7")
 }
